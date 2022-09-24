f89ea1a05d3da1b45d737e339a5fd4d55e7571a3
0S5IgVuwuZtpOksEA5Rie <3QZWZtfVCyVX6v@gmail.com>
2022-09-24 22:06:25 +1000

    work

--- main/Program.cs
+++ main/Program.cs
@@ -4,13 +4,74 @@ namespace main
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.Write("Enter first number: ");
-            var firstNumber = Convert.ToInt64(Console.ReadLine());
+            char[] validOperators = { '+', '-', '*', '/' };
+            bool firstNumberUseArray = false;
+            bool secondNumberUseArray = false;
 
-            Console.Write("Enter operator: ");
-            var @operator = Convert.ToChar(Console.ReadLine());
+            bool firstNumberNegative = false;
+            bool secondNumberNegative = false;
+
+            Console.Write("Enter first number: ");
+            string firstNumberString = Console.ReadLine();
+
+            // Use the smallest memory if possible, if not, keep going larger
+            try
+            {
+                short firstNumber = Convert.ToInt16(firstNumberString);
+            }
+            catch (OverflowException)
+            {
+                try
+                {
+                    int firstNumber = Convert.ToInt32(firstNumberString);
+                }
+                catch (OverflowException)
+                {
+                    try
+                    {
+                        long firstNumber = Convert.ToInt64(firstNumberString);
+                    }
+                    catch (OverflowException)
+                    {
+                        // Since can not go larger with standard integer variable types, turn the large integer into a array
+                        firstNumberUseArray = true;
+                        short[] firstNumber = { };
+                        // Note that it is negative and remove the negative sign
+                        if (firstNumberString[0] == '-')
+                        {
+                            firstNumberNegative = true;
+                            firstNumberString.Substring(1);
+                        }
+                        // Flip the integer around so one's place is at 0, ten's at 1...
+                        for (short placeValue = 0; placeValue < firstNumberString.Length; placeValue++)
+                        {
+                            firstNumber[placeValue] = Convert.ToInt16(firstNumberString[(placeValue + 1) * -1]);
+                        }
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, only insert integers");
+            }
+
+            bool invalidInput = true;
+            while (invalidInput)
+            {
+                Console.Write("Enter operator (+ - * /): ");
+                var @operator = Convert.ToChar(Console.ReadLine());
+                if (@operator in validOperators)
+                {
+                    invalidInput = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input")
+                }
+            }
+
 
             Console.Write("Enter second number: ");
             var secondNumber = Convert.ToInt64(Console.ReadLine());
